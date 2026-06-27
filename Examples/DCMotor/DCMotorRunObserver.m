72bc143f566bd29e1a1bd9991ffdabe7936c90f3
valentin.alleaume <valentin.alleaume@minesparis.psl.eu>
2026-06-27 13:54:44 +0200

    gaussian process

--- Examples/DCMotor/DCMotorRunObserver.m
+++ Examples/DCMotor/DCMotorRunObserver.m
@@ -30,10 +30,10 @@ disp(B);
 aug_sys = AugmentedSystem(obs_sys, 8, A, B);
 
 % Choose an initial condition for the DC motor state and the observer state.
-u_0 = 0.02; % initial input
+u_0 = 1; % initial input
 u_dot_0 = 1; % initial input derivative
 U_pulsation = pi; % pulsation of the input sine wave, we keep it constant for the test
-X0 = [-0.; 0; 0; 0; u_0; u_dot_0; 0.56; 0.23; U_pulsation]; % initial state of the system, we start with q = 1 to see a mode transition in the trajectory
+X0 = [-0.; 0; 0; 0; u_0; u_dot_0; 0.056; 0.023; U_pulsation]; % initial state of the system, we start with q = 1 to see a mode transition in the trajectory
 Z0 = zeros(aug_sys.nz, 1);
 
 % Time spans.
@@ -54,12 +54,45 @@ z = cat(2, z, x(:,5:6), x(:,9)); % we also input u and u_dot to the predictor
 
 t = sol.t(:);
 
+lambda = -3;
+G = [1/sys.L; 0; 0];
+F_1 = [-sys.R/sys.L, -sys.Ke/sys.L, 0; sys.Kt/sys.J, 0, 0; 0, 1, 0];
+H = [0, 1, 0];
+M_1 = H/(F_1 - lambda * eye(3));
+N_1 = M_1*G;
+
+F_0 = [-sys.R/sys.L, -sys.Ke/sys.L, 0; 0, 0, 0; 0, 1, 0];
+M_0 = H/(F_0 - lambda * eye(3));
+N_0 = M_0*G;
+disp(size(M_0));
+disp(size(N_0));
+
+ampl = u_dot_0^2+u_0^2; % amplitude of the input sine wave
+w = ampl/(lambda^2+U_pulsation^2)*(lambda*cos(U_pulsation*t)-U_pulsation*sin(U_pulsation*t)); 
+disp(size(w));
+
+T_0 = M_0*x(:,1:3)' + N_0*w'; % observer correction term for mode 0
+T_1 = M_1*x(:,1:3)' + N_1*w'; % observer correction term for mode 1
+
 figure;
 plot(t, z);
 title('Observer trajectory in z-coordinates', 'Interpreter', 'latex');
 legend(arrayfun(@(k) ['$z_{' num2str(k) '}$'], 1:aug_sys.nz, 'UniformOutput', false), 'Interpreter', 'latex');
 xlabel('Time', 'Interpreter', 'latex');
 grid on;
+
+disp(size(T_0));
+disp(size(T_1));    
+disp(size(z));
+
+figure
+plot(t, T_0, 'LineWidth', 1.5);
+hold on;    
+plot(t, T_1, 'LineWidth', 1.5);
+legend('T mode 0', 'T mode 1', 'Interpreter', 'latex');
+hold on;
+plot(t, z(:,5)', '--', 'LineWidth', 1.5);
+title('Observer in z space', 'Interpreter', 'latex');
 
 % Load a trained predictor if available.
 model_files = dir('ObserverModels/dc-motor-predictor-*.mat');
@@ -70,9 +103,14 @@ models = load(strcat('ObserverModels/', model_files(idx_model).name));
 assert(isequal(A, models.A) && isequal(B, models.B), 'Loaded model does not match current z dynamic.');
 
 % Reconstruct x from z.
+X_pred = zeros(size(x));
+P = 5;
+for i = 1:P
+    X_pred(:, i) = predict(models.mdl{i}, z);
+end
 
 %z = cat(2, z, x(:,5:6)); % we also input u and u_dot to the predictor
-X_pred = predict(models.mdl, (z - models.mu) ./ models.sigma);
+%X_pred = predict(models.mdl, (z - models.mu) ./ models.sigma);
 
 figure;
 plot(x(:, 1), x(:, 2), 'LineWidth', 1.5);
